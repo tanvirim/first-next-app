4bec6747b4590b7a27059e1f1483a2e308b78a8a
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-11-03 23:35:00 +0600

    next api and error

---
+++ app/users/error.tsx
@@ -0,0 +1,13 @@
+// pages/404.tsx
+
+'use client';
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you are looking for does not exist.</p>
+      {/* Additional content or links can be added here */}
+    </div>
+  );
+};
+export default NotFound;
